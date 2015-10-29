a0f7b5a86fb46a4a7e6132ea8430be8583ffddaf
Will Hodges <hodges.wt@gmail.com>
2015-10-29 13:57:56 -0400

    Update uninstall.sh
    Fixed if/else errors and permissions problems.

--- uninstall.sh
+++ uninstall.sh
@@ -1,27 +1,29 @@
+#!/bin/sh
+
 echo "If you would like to uninstall Android Studio, all of its projects, all gradle files, all simulators, and SDK tools, type '1'. If you would like to uninstall just Android Studio, type '2'. To cancel, type '3'."
 read option
 echo "You chose option $option"
-if ["$option" == "1"]; then
-	`rm -Rf /Applications/Android\ Studio.app`
-	`rm -Rf ~/Library/Preferences/AndroidStudio*`
-	`rm ~/Library/Preferences/com.google.android.studio.plist`
-	`rm -Rf ~/Library/Application\ Support/AndroidStudio*`
-	`rm -Rf ~/Library/Logs/AndroidStudio*`
-	`rm -Rf ~/Library/Caches/AndroidStudio*`
-	`rm -Rf ~/AndroidStudioProjects`
-	`rm -Rf ~/.gradle`
-	`rm -Rf ~/.android`
-	`rm -Rf ~/Library/Android*`
+if [ "$option" == "1" ]; then
+	`sudo rm -Rf /Applications/Android\ Studio.app`
+	`sudo rm -Rf ~/Library/Preferences/AndroidStudio*`
+	`sudo rm ~/Library/Preferences/com.google.android.studio.plist`
+	`sudo rm -Rf ~/Library/Application\ Support/AndroidStudio*`
+	`sudo rm -Rf ~/Library/Logs/AndroidStudio*`
+	`sudo rm -Rf ~/Library/Caches/AndroidStudio*`
+	`sudo rm -Rf ~/AndroidStudioProjects`
+	`sudo rm -Rf ~/.gradle`
+	`sudo rm -Rf ~/.android`
+	`sudo rm -Rf ~/Library/Android*`
 	echo "Uninstalled everything related to Android Development on your Mac!"
-elif ["$option" == "2"]; then
-	`rm -Rf /Applications/Android\ Studio.app`
-	`rm -Rf ~/Library/Preferences/AndroidStudio*`
-	`rm ~/Library/Preferences/com.google.android.studio.plist`
-	`rm -Rf ~/Library/Application\ Support/AndroidStudio*`
-	`rm -Rf ~/Library/Logs/AndroidStudio*`
-	`rm -Rf ~/Library/Caches/AndroidStudio*`
+elif [ "$option" == "2" ]; then
+	`sudo rm -Rf /Applications/Android\ Studio.app`
+	`sudo rm -Rf ~/Library/Preferences/AndroidStudio*`
+	`sudo rm ~/Library/Preferences/com.google.android.studio.plist`
+	`sudo rm -Rf ~/Library/Application\ Support/AndroidStudio*`
+	`sudo rm -Rf ~/Library/Logs/AndroidStudio*`
+	`sudo rm -Rf ~/Library/Caches/AndroidStudio*`
 	echo "Uninstalled everything related to Android Studio, but not Android Development!"
-elif ["$option" == "3"]; then
+elif [ "$option" == "3" ]; then
 	echo "Bye, bye!"
 	exit 0
 else
